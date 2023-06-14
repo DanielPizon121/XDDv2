999a1973b777b5a74aeb070ba91ea34f9871536b
DanielPizon121 <135995570+DanielPizon121@users.noreply.github.com>
2023-06-14 14:45:54 +0200

    Update Main.java
    git jest

--- Main.java
+++ Main.java
@@ -20,3 +20,26 @@ public class Main {
         System.out.println("Koniec programu.");
     }
 }
+
+
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Podaj liczbę:");
+
+        String input = scanner.nextLine();
+
+        try {
+            int number = Integer.parseInt(input);
+            System.out.println("Podana liczba " + number + " jest całkowita.");
+        } catch (NumberFormatException e) {
+            System.out.println("Podana wartość nie jest liczbą całkowitą.");
+        }
+
+        scanner.close();
+    }
+}
