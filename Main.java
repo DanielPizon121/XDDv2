b68cc66dde5d5683f35145abc63880a8d0431a41
DanielPizon121 <135995570+DanielPizon121@users.noreply.github.com>
2023-06-14 14:31:07 +0200

    Update Main.java
    idk czy o to chodziło

--- Main.java
+++ Main.java
@@ -1,7 +1,22 @@
-class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
-  }
-}
+import java.util.Scanner;
 
-Dupa
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Wprowadzaj znaki (naciśnij 'k' aby zakończyć):");
+
+        while (true) {
+            String input = scanner.nextLine();
+
+            if (input.equals("k")) {
+                break;
+            }
+
+            System.out.println("Wprowadzono: " + input);
+        }
+
+        scanner.close();
+        System.out.println("Koniec programu.");
+    }
+}
